142e23af68b07d6e22eb4554a633910c2a3ebf86
Martin Langa <167089903+elverolanga@users.noreply.github.com>
2025-02-12 11:13:30 +0200

    fix: bugs

--- src/assets/data.js
+++ src/assets/data.js
@@ -2,20 +2,20 @@ export const FavPics = [
     {
         id: 1,
         title: "Nem preciso falar muito",
-        src: './src/assets/pics/dummy.png',
+        src: './pics/dummy.png',
         class: 'Card'
     },
     {
         id: 2,
         title: "Uma maça por dia, traz saude e alegría",
-        src:'./src/assets/pics/mango.png',
+        src:'./pics/mango.png',
         description: "",
         class: 'Card'
     },
     {
         id: 3,
         title: "Sinceramente não sei oque se passa contigo",
-        src:'./src/assets/pics/idk.png',
+        src:'./pics/idk.png',
         description: "",
         class: 'Card'
     }
@@ -24,38 +24,38 @@ export const FavPics = [
 export const FavMoments = [
     {
         id: 4,
-        src: './src/assets/pics/luv.png',
+        src: './pics/luv.png',
         title: 'The Day you said Yes',
         class: 'CardV'
     },
     {
         
         id: 5,
-        src: './src/assets/pics/brazy.png',
+        src: './pics/brazy.png',
         title: 'Depois de acabar as coxinhas',
         class: 'CardV'
     },
     {
         id: 6,
-        src: './src/assets/pics/happysad.png',
+        src: './pics/happysad.png',
         title: 'A happy, sad moment',
         class: 'CardV'
     },
     {
         id: 7,
-        src: './src/assets/pics/10km.png',
+        src: './pics/10km.png',
         title: 'O dia que o tal Demoniozinho te fez andar',
         class: 'CardV'
     },
     {
         id: 8,
-        src: './src/assets/pics/church.png',
+        src: './pics/church.png',
         title: 'Momentos antes',
         class: 'CardV'
     },
     {
         id: 9,
-        src: './src/assets/pics/bruhhhhhh.png',
+        src: './pics/bruhhhhhh.png',
         title: 'Locurasssss',
         class: 'CardV'
     }
